ddca2b1762951afc7ab6148d7024bc35d17381c5
pseddonxvi <spacexplorer.xvi@gmail.com>
2024-06-25 13:11:58 -0500

    Added code and Libraries

---
+++ code.py
@@ -0,0 +1,43 @@
+# Get signal from a button
+# One pole to 3.3V and one to a GPIO pin
+
+import board
+import time
+import digitalio
+import board
+import neopixel
+from ledPixelsPico import *
+
+# Defines state of LEDs 
+def turnon():
+    for i in range (8):
+        pixels[i] = (220, 220, 190)
+        
+def turnoff():
+    for i in range (8):
+        pixels[i] = (0, 0, 0)
+
+# Chnage GPIO for LEDs here
+pixels = neopixel.NeoPixel(board.GP22, 8)
+
+# Change GPIO for button here
+timerBtn = digitalio.DigitalInOut(board.GP10)
+timerBtn.pull = digitalio.Pull.DOWN
+
+mode="off"
+
+#Controls how switch behaves
+while True:
+    if timerBtn.value:
+        while timerBtn.value:
+            time.sleep(0.1)
+        if mode == "off":
+            mode="on"
+            turnon()
+        else:
+            mode="off"
+            turnoff()
+        print("mode:", mode)
+
+    
+        
